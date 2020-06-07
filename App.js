37d1ff92b5c64cce1462fabe55d09a99d7b0a308
Harman <s.harmanjot2007@gmail.com>
2020-06-07 08:22:34 +0530

    Changes Done

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
 import WriteScreen from './screens/WriteScreen';
