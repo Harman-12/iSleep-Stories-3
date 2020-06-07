97684479848b50a52e1ed1cfb88ab1fb923d0b32
Harman <s.harmanjot2007@gmail.com>
2020-06-07 08:11:12 +0530

    Added Changes

--- App.js
+++ App.js
@@ -16,7 +16,34 @@ export default class App extends React.Component  {
 const TabNavigator = createBottomTabNavigator({
   WriteStory: {screen: WriteScreen},
   ReadStory: {screen: ReadScreen},
-});
+},
+{
+  defaultNavigationOptions: ({navigation})=>({
+    tabBarIcon: ()=>{
+      const routeName = navigation.state.routeName;
+      console.log(routeName)
+      if(routeName === "WriteStory"){
+        return(
+          <Image
+          source={require("./assets/write.png")}
+          style={{width:40, height:40}}
+        />
+        )
+        
+      }
+      else if(routeName === "ReadStory"){
+        return(
+          <Image
+          source={require("./assets/read.png")}
+          style={{width:40, height:40}}
+        />
+        )
+        
+      }
+    }
+  })
+}
+)
 
 const AppContainer = createAppContainer(TabNavigator);
 
